02963f6318d12603b0e8ef8f616aebdac79570e4
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-02-15 23:25:01 +0000

    Create order summary with custom Button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -16,7 +17,16 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
+      <Button btnStyle="Danger" clicked={props.purchaseCancelled}>
+        Cancel
+      </Button>
+      <Button btnStyle="Success" clicked={props.purchaseContinued}>
+        Checkout
+      </Button>
     </Aux>
   );
 };
