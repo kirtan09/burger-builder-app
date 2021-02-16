909fd4bb1048d8da24881ea666b2dca74c97eb98
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-02-16 22:44:23 +0000

    Only update component when modal is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnStyle="danger" clicked={props.purchaseCancelled}>
-        Cancel
-      </Button>
-      <Button btnStyle="success" clicked={props.purchaseContinued}>
-        Checkout
-      </Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnStyle="danger" clicked={this.props.purchaseCancelled}>
+          Cancel
+        </Button>
+        <Button btnStyle="success" clicked={this.props.purchaseContinued}>
+          Checkout
+        </Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
