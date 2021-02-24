2c4320a9d56757367160d036edcd8531acf7df3b
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-02-24 19:42:07 +0000

    Creating account via firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ0-ThUjwvnct12MockCVvLalfk9av57g",
+  authDomain: "burger-builder-k09.firebaseapp.com",
+  databaseURL: "https://burger-builder-k09-default-rtdb.firebaseio.com",
+  projectId: "burger-builder-k09",
+  storageBucket: "burger-builder-k09.appspot.com",
+  messagingSenderId: "960107807935",
+  appId: "1:960107807935:web:22d07edebaf28af043f20a",
+};
+
+export default firebaseConfig;
