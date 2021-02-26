86d759457d371722b0a446686e765280c8b22c7e
kirtan09 <35260448+kirtan09@users.noreply.github.com>
2021-02-26 22:15:00 +0000

    adding persistent login state and storing in localstorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/logout" component={Logout} />
-        </Switch>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onAuthCheckState();
+  }
+  render() {
+    let routes = this.props.isAuth ? (
+      <>
+        <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />{" "}
+      </>
+    ) : null;
+
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path="/auth" component={Auth} />
+            {routes}
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.auth.token !== null,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuthCheckState: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
